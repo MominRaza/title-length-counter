f047439c9e3d3ee40a66603ee0fb7521ca1e30fa
MominRaza <mominr082@gmail.com>
2021-01-05 11:58:21 +0530

    added js to count

--- title-length-counter-layer.php
+++ title-length-counter-layer.php
@@ -2,5 +2,14 @@
 
 class qa_html_theme_layer extends qa_html_theme_base
 {
-    
+    function body_suffix()
+	{
+        qa_html_theme_base::body_suffix();
+        $this->output('<script type="text/javascript">        
+var title = document.getElementById("title");
+title.onkeyup = function () {
+    console.log(title.value.length);
+}
+</script>');
+    }
 }
